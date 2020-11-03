b24148aaeadb2be2f988e32d9aa478fc3bd9f7d1
郭建林 <1050070285@qq.com>
2020-11-03 18:31:19 +0800

    点赞组件初步完成,但是加上socped属性就不能显示了

---
+++ src/components/give-the-thumbs-up.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="dzDiv">
+    <!--    /*直播点赞动画*/-->
+    <div class="btn" @click="like">点赞</div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'GiveTheThumbsUp',
+  data() {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    like() {
+      const dzDiv = document.querySelector('.dzDiv')
+      const likeDom = document.createElement('div')
+      this.count++
+      console.log(this.count)
+      likeDom.className = this.count % 2 === 0 ? 'like' : 'like like--is-second'
+      likeDom.style.willChange = 'margin-top'
+      dzDiv.appendChild(likeDom)
+      setTimeout(() => {
+        dzDiv.removeChild(likeDom)
+      }, 3900)
+    }
+  }
+}
+</script>
+
+<style  lang="scss">
+  @import "src/components/give-the-thumbs-up.scss";
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,18 @@
 <template>
-  <div class="home" />
+  <div class="home">
+    <givethethumbsup />
+  </div>
 </template>
 
 <script>
-
+import givethethumbsup from '../components/give-the-thumbs-up'
 export default {
   name: 'Home',
   components: {
-
+    givethethumbsup
   }
 }
 </script>
+<style scoped lang="scss">
+
+</style>
